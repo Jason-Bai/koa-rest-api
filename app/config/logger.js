131d19b6036d4610abbe10c6324545e22702a183
baiyu <baiyu@admaster.com.cn>
2018-07-25 18:24:25 +0800

    add bunyan-logstash

--- app/config/logger.js
+++ app/config/logger.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const bunyanLogStash = require('bunyan-logstash');
 const { env, logger } = require('./');
 
 
@@ -9,13 +10,31 @@ const filename = process.env.LOG_FILENAME || `${logger.name}.${env}.json.log`;
 
 // Add streams as depending on the environment
 if (env === 'production') {
-  logger.streams.push({
-    type: 'rotating-file',
-    path: path.join(directory, filename),
-    period: '1d',
-    count: 7,
-    level: process.env.LOG_LEVEL || 'info'
-  });
+  const isFile = process.env.LOG_TYPE === 'file';
+
+  if (isFile) {
+    // output to file
+    logger.streams.push({
+      type: 'rotating-file',
+      path: path.join(directory, filename),
+      period: '1d',
+      count: 7,
+      level: process.env.LOG_LEVEL || 'info'
+    });
+  } else {
+    // output to logstash
+    logger.streams.push({
+      type: 'raw',
+      stream: bunyanLogStash.createStream({
+        application: process.env.APP_NAME,
+        host: process.env.LOGSTASH_HOST || '127.0.0.1',
+        port: process.env.LOGSTASH_PORT || '5044',
+        level: process.env.LOG_LEVEL || 'info'
+      }),
+    });
+  }
+
+
   logger.streams.push({
     type: 'stream',
     stream: process.stderr,
